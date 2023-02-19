f885231366c1a12002367cf5a01b7e8a581cb4db
arinas <andreas_rinas@web.de>
2023-02-19 22:51:27 +0100

    game loop

--- Game/src/de/learngamedev/Display.java
+++ Game/src/de/learngamedev/Display.java
@@ -3,13 +3,46 @@ package de.learngamedev;
 import javax.swing.*;
 import java.awt.*;
 
-public class Display extends Canvas {
+public class Display extends Canvas implements Runnable {
 
     private static final long serialVersionUID = 1L;
-   public static  final int WIDTH = 800;
+    public static  final int WIDTH = 800;
     public static final int HEIGHT = 600;
 
     public static final String TITLE = "MyFirstGame";
+
+
+
+    private Thread mainThread;
+    private boolean running = false;
+
+    private void start() {
+        if(running) return;
+        else {
+            running = true;
+            mainThread = new Thread(this);
+            mainThread.start();
+        }
+    }
+
+    private void stop() {
+        if (!running) return;
+        else {
+            running = false;
+            try {
+                mainThread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                System.exit(-1);
+            }
+        }
+    }
+
+    public void run() {
+        while (running) {
+
+        }
+    }
 
     public static void main(String[] args) {
         Display game = new Display();
@@ -22,6 +55,7 @@ public class Display extends Canvas {
         frame.setTitle(TITLE);
         frame.setResizable(false);
         frame.setVisible(true);
+        game.start();
 
         System.out.println("Game running...");
     }
